b69e0b662147ae672d6f2be233d77ef8c95857a0
Nessvh <vanessamarquesns@outlook.pt>
2021-08-13 16:56:15 +0100

    Fix variable to store minutes

--- convertminutes.c
+++ convertminutes.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     //Create a variable to store the minutes the user will give us
-    char *numberOfMinutes = argv[1];
+    int minutes = atoi(argv[1]);
+
+    //Create variables to store the information that we need
+    
+    double minutesInYears;
+    double days;
+    double years;
 
 
-
-    printf("Number of minutes entered: %s\n", numberOfMinutes);
+    printf("Number of minutes are: %d\n", minutes);
 
     return 0;
 }
