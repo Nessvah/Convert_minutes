553c215a0ca7040d2469dd41580f4263270c81fd
Nessvh <vanessamarquesns@outlook.pt>
2021-08-13 17:09:37 +0100

    Calculate minutes in year

--- convertminutes.c
+++ convertminutes.c
@@ -4,16 +4,25 @@
 int main(int argc, char *argv[])
 {
     //Create a variable to store the minutes the user will give us
+    //This will convert argv char to an int
     int minutes = atoi(argv[1]);
 
-    //Create variables to store the information that we need
-    
+    //Create variables to store the rest of information that we need
     double minutesInYears;
     double days;
     double years;
 
+    //Calculate minutes in 1 Year (365)
+    int oneHour, oneDay, oneYear;
 
-    printf("Number of minutes are: %d\n", minutes);
+    oneHour = 60;
+    oneDay = 24; 
+    oneYear = 365; 
+
+    minutesInYears = (60 * 24) * 365;
+
+
+    printf("Number of minutes in a year are: %.2f\n", minutesInYears);
 
     return 0;
 }
