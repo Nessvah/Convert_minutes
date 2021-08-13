06ec62cd43ba6d5ec95c9da1af752a7151fcd1ef
Nessvh <vanessamarquesns@outlook.pt>
2021-08-13 16:41:17 +0100

    Create variable to number of minutes

--- convertminutes.c
+++ convertminutes.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-int main()
-
+int main(int argc, char *argv[])
 {
-    printf("First commit.\n");
+    //Create a variable to store the minutes the user will give us
+    char *numberOfMinutes = argv[1];
+
+
+
+    printf("Number of minutes entered: %s\n", numberOfMinutes);
 
     return 0;
 }
