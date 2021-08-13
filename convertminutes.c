3d475d18ab81acbd1bc8bf2999f16c3dd078fb5a
Nessvh <vanessamarquesns@outlook.pt>
2021-08-13 17:53:38 +0100

    Convert minutes to days and years

--- convertminutes.c
+++ convertminutes.c
@@ -8,9 +8,7 @@ int main(int argc, char *argv[])
     int minutes = atoi(argv[1]);
 
     //Create variables to store the rest of information that we need
-    double minutesInYears;
-    double days;
-    double years;
+    double minutesInYears, days, years;
 
     //Calculate minutes in 1 Year (365)
     int oneHour, oneDay, oneYear;
@@ -21,11 +19,14 @@ int main(int argc, char *argv[])
 
     minutesInYears = (60 * 24) * 365;
 
-    //Formula to convert minutes into years
-    years = minutes / minutesInYears; 
+    //Convert number of minutes into days and years
+    days = minutes / (60 * 24);
 
+    years = minutes / minutesInYears;
 
-    printf("Number of years is: %.2f\n", years);
+    //Output data
+
+    printf("You entered %d minutes.\nThis corresponds to %.2f days the equivalent to %.2f years.\n", minutes, days, years);
 
     return 0;
 }
