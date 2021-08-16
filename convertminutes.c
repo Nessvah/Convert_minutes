63ab9d939e671f3431c58243e9be17c3bd210fef
Nessvh <vanessamarquesns@outlook.pt>
2021-08-16 12:13:38 +0100

    Use Scanf instead

--- convertminutes.c
+++ convertminutes.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    //Create a variable to store the minutes the user will give us
-    //This will convert argv char to an int
-    int minutes = atoi(argv[1]);
-
-    //Create variables to store the rest of information that we need
+    //Create variables to store the information that we need
     double minutesInYears, days, years;
+    int minutesEntered;
 
+
+    //Ask the user for input
+    printf("Please enter number of minutes: \n");
+    scanf("%d", &minutesEntered);
+    
     //Calculate minutes in 1 Year (365)
-    int oneHour, oneDay, oneYear;
-
-    oneHour = 60;
-    oneDay = 24; 
-    oneYear = 365; 
 
     minutesInYears = (60 * 24) * 365;
 
     //Convert number of minutes into days and years
-    days = minutes / (60 * 24);
+    days = minutesEntered / (double)(60 * 24);
 
-    years = minutes / minutesInYears;
+    years = minutesEntered / minutesInYears;
 
     //Output data
 
-    printf("You entered %d minutes.\nThis corresponds to %.2f days the equivalent to %.2f years.\n", minutes, days, years);
+    printf("You entered %d minutes.\nThis corresponds to %.2f days the equivalent to %f years.\n", minutesEntered, days, years);
 
     return 0;
 }
